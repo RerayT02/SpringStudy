71680222c20c1326eb073aa2d789130264a97116
RerayT02 <731778906@qq.com>
2025-10-29 18:46:37 +0800

    feat(wangpu): spring 示例代码

---
+++ springstudy1/src/main/java/com/reray/spring/study/MySpringBean.java
@@ -0,0 +1,17 @@
+package com.reray.spring.study;
+
+/**
+ * @author pu wang
+ * @date 2025/10/29 17:07
+ */
+public class MySpringBean {
+    private String beanName = "beanName";
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+}
